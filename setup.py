0bebe4769df21a6065c1c4fad9c3a8a305a124f5
noraj <alexandre.zanni@europe.com>
2019-05-17 00:47:31 +0200

    better packaging

--- setup.py
+++ setup.py
@@ -12,14 +12,18 @@ import codecs
 read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
 
 if 'publish' in sys.argv:
-    os.system('python setup.py sdist upload')
+    os.system('python -m twine upload dist/*')
+    sys.exit()
+if 'package' in sys.argv:
+    os.system('python setup.py bdist bdist_egg')
     sys.exit()
 
 setup(
     name='flask-session-cookie-manager',
-    version='1.2.1',
+    version='1.2.1.1',
     description="simple Python script to deal with Flask session cookie",
-    #long_description=read(join(dirname(__file__), 'README.md')),
+    long_description=read(join(dirname(__file__), 'README.md')),
+    long_description_content_type='text/markdown',
     keywords='Flask session cookie',
     author='Wilson Sumanang, Alexandre ZANNI',
     maintainer='Alexandre ZANNI, TAbdiukov',
